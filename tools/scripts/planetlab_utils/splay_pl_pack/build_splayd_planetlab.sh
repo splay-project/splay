d232b3d8389e18cb83ee2bfb023a16e7e988e8f8
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-06-15 14:24:13 +0000

    do not include test files from PL distrib

--- tools/scripts/planetlab_utils/splay_pl_pack/build_splayd_planetlab.sh
+++ tools/scripts/planetlab_utils/splay_pl_pack/build_splayd_planetlab.sh
@@ -5,6 +5,14 @@
 cp ../../../../src/daemon/modules/*.lua splayd_planetlab/splayd/modules/
 cp ../../../../src/daemon/modules/splay/*.lua splayd_planetlab/splayd/modules/splay/
 
+##test files do not go into distribution
+rm splayd_planetlab/splayd/modules/splay/test-dns.lua
+rm splayd_planetlab/splayd/modules/splay/test-misc.lua
+rm splayd_planetlab/splayd/modules/splay/test-splay-async-dns.lua
+rm splayd_planetlab/splayd/modules/splay/test-benc.lua
+rm splayd_planetlab/splayd/modules/splay/test-async_dns.lua
+
+
 COPY_EXTENDED_ATTRIBUTES_DISABLE=true COPYFILE_DISABLE=true tar czvf "splayd_planetlab.tar.gz"  --exclude=.svn splayd_planetlab/
 
 echo "Splayd4PL built in splayd_planetlab.tar.gz"
