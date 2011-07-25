3210d4a4d0f68872a20788f91e82d9963224ae6a
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-07-25 14:51:05 +0000

    fix path after machine upgrade

--- tools/scripts/planetlab_utils/splay_pl_pack/build_deploy_run.sh
+++ tools/scripts/planetlab_utils/splay_pl_pack/build_deploy_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./build_splayd_planetlab.sh
-scp splayd_planetlab.tar.gz root@splay2.unineuchatel.ch:/splay/splayweb/public/
-scp local_install.sh root@splay2.unineuchatel.ch:/splay/splayweb/public/
+scp splayd_planetlab.tar.gz root@splay2.unineuchatel.ch:/home/splay/splayweb_1.0.2/public
+scp local_install.sh root@splay2.unineuchatel.ch:/home/splay/splayweb_1.0.2/public
 #./install.sh ../unineple_splay_nodes.txt
