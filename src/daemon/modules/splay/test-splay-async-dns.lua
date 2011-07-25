97ce3b0ff030cb70937c801654ea71258461103a
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-07-25 14:05:59 +0000

    add restrictions to socket

--- src/daemon/modules/splay/test-splay-async-dns.lua
+++ src/daemon/modules/splay/test-splay-async-dns.lua
@@ -20,16 +20,26 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Splayd. If not, see <http://www.gnu.org/licenses/>.
 ]]
+socket = require"socket.core"
+rs=require"splay.restricted_socket"
+rs.l_o.level=1
+rs.init(
+	{max_sockets=1024,
+	local_ip="127.0.0.1",
+	start_port=11000,end_port=11500}
+)
+socket=rs.wrap(socket)
 
 require"splay.base"
 
 events.run(function()
 	local ip,_ = socket.dns.toip("orion.unine.ch")
 	assert(ip=="130.125.1.11","Expected 130.125.1.11 but was "..ip)
-	print("orion.unine.ch ->"..ip)
+	--print("orion.unine.ch ->"..ip)
 	
 	local name,_ = socket.dns.tohostname("130.125.1.11")
 	--the '.' at the end of the domain is intended by the DNS RFC
 	assert(name=="orion.unine.ch.","Expected orion.unine.ch. but was "..name)	
-	print("130.125.1.11 ->"..name)
+	--print("130.125.1.11 ->"..name)
+
 end)
