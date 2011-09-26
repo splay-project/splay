ba405b270549e8b2fc54e04986eb9ea904a1e4a6
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-09-26 08:51:03 +0000

    checks for input

--- src/replace-version-splayweb.sh
+++ src/replace-version-splayweb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [[ $1 == "" ]]; then
+	echo "Usage: ./replace-version-splayweb.sh NEW_VERSION"
+	exit
+fi
 new_version=$1
 
 find splayweb -name "*.rb" -exec ./change-header-splayweb.sh {} ${new_version} \;
