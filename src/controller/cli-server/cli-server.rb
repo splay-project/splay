f4cdd2bf9231693079f0014ad9849f134817f039
Jose Valerio <jose.valerio@unine.ch>
2011-10-03 14:51:30 +0000

    adding a lot of changes in cli-server/client phase1: common variables go to lib/common.lua, new way of showing that cli-server, usename are taken from conf file, url changed from rpc-server to splay-ctrl-api

--- src/controller/cli-server/cli-server.rb
+++ src/controller/cli-server/cli-server.rb
@@ -2,21 +2,21 @@
 ## Splay Controller ### v1.1 ###
 ## Copyright 2006-2011
 ## http://www.splay-project.org
-## 
-## 
-## 
+##
+##
+##
 ## This file is part of Splay.
-## 
-## Splayd is free software: you can redistribute it and/or modify 
-## it under the terms of the GNU General Public License as published 
-## by the Free Software Foundation, either version 3 of the License, 
+##
+## Splayd is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License,
 ## or (at your option) any later version.
-## 
-## Splayd is distributed in the hope that it will be useful,but 
+##
+## Splayd is distributed in the hope that it will be useful,but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ## See the GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with Splayd. If not, see <http://www.gnu.org/licenses/>.
 
@@ -35,14 +35,14 @@ $logger.level = Logger::DEBUG
 s = HTTPServer.new( :Port => 2222,
                     :DocumentRoot  =>  File.dirname( __FILE__ )  )
 
-s.mount("/json-rpc", Orbjson::WEBrick_JSON_RPC )
+s.mount("/splay-ctrl-api", Orbjson::WEBrick_JSON_RPC )
 
 
 # These mounts are a handy way to to kill a WEBrick instance via a URL call,
 # useful mostly during debugging.  Probably less useful when you've deployed
 # the applicaton.  Consider removing these before going live.
-# s.mount_proc( "/exit" ){|req, res| s.shutdown;  exit;  } 
-# s.mount_proc( "/quit" ){|req, res| s.shutdown;  exit; }   
+# s.mount_proc( "/exit" ){|req, res| s.shutdown;  exit;  }
+# s.mount_proc( "/quit" ){|req, res| s.shutdown;  exit; }
 
 
 # You can configure the Orbjsn services list in a few ways.
@@ -51,13 +51,13 @@ s.mount("/json-rpc", Orbjson::WEBrick_JSON_RPC )
 #   'file://'
 #     For example: cfg = 'file://config.yaml'
 #     Orbjson::System.init( cfg )
-#     
-#  2. Pass in some YAML:   
-#    cfg = 'services/sample: 
+#
+#  2. Pass in some YAML:
+#    cfg = 'services/sample:
 #            - Details'
 #    Orbjson::System.init( cfg )
-#            
-#  3. Pass in some an axtual Hash object: 
+#
+#  3. Pass in some an axtual Hash object:
 #    cfg = { 'services/sample' => ['Details'] }
 #    Orbjson::System.init( cfg )
 #
@@ -68,7 +68,7 @@ s.mount("/json-rpc", Orbjson::WEBrick_JSON_RPC )
 # Change this to suit your actual situation: If you use a configuration file,
 # # make sure you have the correct name, path, and contents.
 # This example expects to find the file 'config.yml' in the same dir
-# as server.rb 
+# as server.rb
 cfg = { 'controller-api' => ['Ctrl_api'] }
 #cfg = "file://config.yml"
 
