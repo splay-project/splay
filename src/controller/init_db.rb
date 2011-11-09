2b23b4b924e831f5cbc4ffd9952bf34b9693afbf
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-11-09 15:13:57 +0000

    remove lib/json.rb, use gem json 1.6.1; cosmetic changes in requirements to improve compatibility with future support to Ruby 1.9.x

--- src/controller/init_db.rb
+++ src/controller/init_db.rb
@@ -24,7 +24,7 @@
 
 # GRANT ALL PRIVILEGES ON splay.* TO splay@localhost IDENTIFIED BY 'splay';
 
-require 'lib/all'
+require File.expand_path(File.join(File.dirname(__FILE__), 'lib/all'))
 
 def drop_db(db)
 	db.do("DROP TABLE IF EXISTS splayds")
