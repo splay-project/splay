1f3c107ad8b4bb89435533e850626937c7c5508a
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-12-08 16:09:36 +0000

    replaced by bump_version_client_commands, 1 file to rule them all

--- src/replace-version-client-commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "" ]]; then
-	echo "Usage: ./replace-version-client-commands.sh NEW_VERSION"
-	exit
-fi
-new_version=$1
-
-find rpc_client -name "*.lua" -exec ./change-header-client-commands.sh {} ${new_version} \;
