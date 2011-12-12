f17f6774aa2de87ac88a27605c68d68193fbd4d0
Lucas Charles <lucas.charles@unine.ch>
2011-12-12 12:43:50 +0000

    secure access to field lib_name that might not be correctly initialized

--- src/daemon/jobd.lua
+++ src/daemon/jobd.lua
@@ -218,8 +218,12 @@ require"splay.coxpcall"
 _sand_check = true
 sandbox = require"splay.sandbox"
 local sd=sandbox.sandboxed_denied --stub for sand'ed functions
+if job.lib_name ~= nil and job.lib_name ~= "" then
 local native_from_job = string.sub(job.lib_name,0,(#(job.lib_name) -3))
-print("Allow lib "..native_from_job,job.lib_version)
+	print("Allow lib "..native_from_job,job.lib_version)
+else
+	print("no lib to add")
+end
 
 
 sandbox.protect_env({
