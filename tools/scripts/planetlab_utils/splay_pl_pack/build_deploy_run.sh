fd8fa1c998ebc425194caf8d17f50d20b0911ad4
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2011-12-12 09:58:28 +0000

    build somewhere else

--- tools/scripts/planetlab_utils/splay_pl_pack/build_deploy_run.sh
+++ tools/scripts/planetlab_utils/splay_pl_pack/build_deploy_run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-./build_splayd_planetlab.sh
 scp splayd_planetlab.tar.gz splay@splay2.unineuchatel.ch:/home/splay/splayweb_1.1/public
 scp local_install.sh splay@splay2.unineuchatel.ch:/home/splay/splayweb_1.1/public
-#./install.sh ../unineple_splay_nodes.txt
