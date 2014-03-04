cd774dabffe6f92b3e2b669f5e4a7cf0de68699e
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-03-04 15:01:17 +0100

    move pkg payload into PKG_PAYLOAD, address #3

--- src/osx-installer/update_content_pkg.sh
+++ src/osx-installer/update_content_pkg.sh
@@ -8,26 +8,27 @@ if [[ $VER == "" ]]; then
 	exit
 fi
 
-wget http://www.splay-project.org/splay/release/splayd_${VER}.tar.gz
+#wget http://www.splay-project.org/splay/release/splayd_${VER}.tar.gz
+cp ../splayd_${VER}.tar.gz .
 tar xzvf splayd_${VER}.tar.gz
 cd splayd_${VER}
 make -f Makefile.macosx splayd jobd splay_core.so misc_core.so data_bits_core.so luacrypto/crypto.so 
 cd ..
 
-cp splayd_${VER}/splayd SPLAY/dist/
-cp splayd_${VER}/splayd.lua SPLAY/dist/
-cp splayd_${VER}/jobd SPLAY/dist/
-cp splayd_${VER}/jobd.lua SPLAY/dist/
-cp splayd_${VER}/settings.lua SPLAY/dist/
+cp splayd_${VER}/splayd PKG_PAYLOAD/SPLAY/dist/
+cp splayd_${VER}/splayd.lua PKG_PAYLOAD/SPLAY/dist/
+cp splayd_${VER}/jobd PKG_PAYLOAD/SPLAY/dist/
+cp splayd_${VER}/jobd.lua PKG_PAYLOAD/SPLAY/dist/
+cp splayd_${VER}/settings.lua PKG_PAYLOAD/SPLAY/dist/
 
-cp splayd_${VER}/splay_core.so SPLAY/clibs/
-cp splayd_${VER}/misc_core.so SPLAY/clibs/splay/
-cp splayd_${VER}/data_bits_core.so SPLAY/clibs/splay/
-cp splayd_${VER}/luacrypto/crypto.so SPLAY/clibs/splay/
+cp splayd_${VER}/splay_core.so PKG_PAYLOAD/SPLAY/clibs/
+cp splayd_${VER}/misc_core.so PKG_PAYLOAD/SPLAY/clibs/splay/
+cp splayd_${VER}/data_bits_core.so PKG_PAYLOAD/SPLAY/clibs/splay/
+cp splayd_${VER}/luacrypto/crypto.so PKG_PAYLOAD/SPLAY/clibs/splay/
 
-cp splayd_${VER}/modules/json.lua SPLAY/lualibs/
-cp splayd_${VER}/modules/splay.lua SPLAY/lualibs/
-cp splayd_${VER}/modules/splay/*lua SPLAY/lualibs/splay/
+cp splayd_${VER}/modules/json.lua PKG_PAYLOAD/SPLAY/lualibs/
+cp splayd_${VER}/modules/splay.lua PKG_PAYLOAD/SPLAY/lualibs/
+cp splayd_${VER}/modules/splay/*lua PKG_PAYLOAD/SPLAY/lualibs/splay/
 
 rm -rf splayd_${VER}.tar.gz
 rm -rf splayd_${VER}/
