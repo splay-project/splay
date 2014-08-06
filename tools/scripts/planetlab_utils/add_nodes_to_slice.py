27cc9d56edc71fa8dfabce2f87df443507015762
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-08-06 18:07:06 +0200

    take list of nodes from arguments, remove unsupportd queries used to query comon

--- tools/scripts/planetlab_utils/add_nodes_to_slice.py
+++ tools/scripts/planetlab_utils/add_nodes_to_slice.py
@@ -4,20 +4,16 @@ import xmlrpclib
 import sys
 import urllib
 
-if len(sys.argv) != 4:
-	print "Usage: ./add_nodes_to_slice.py pl_username pl_pwd pl_slice_name"
+if len(sys.argv) != 5:
+	print "Usage: ./add_nodes_to_slice.py pl_username pl_pwd pl_slice_name list_of_nodes.txt"
 	sys.exit(2)
 
-all_alive_nodes_query = "http://comon.cs.princeton.edu/status/tabulator.cgi?table=table_nodeviewshort&format=nameonly"
-alive_nodes_query = "http://comon.cs.princeton.edu/status/tabulator.cgi?table=table_nodeviewshort&format=nameonly&persite=2&select='resptime>0"
-alive_no_problems="http://comon.cs.princeton.edu/status/tabulator.cgi?format=nameonly&table=table_nodeviewshort&select='resptime%20%3E%200%20&&%20((drift%20%3E%201m%20||%20(dns1udp%20%3E%2080%20&&%20dns2udp%20%3E%2080)%20||%20gbfree%20%3C%205%20||%20sshstatus%20%3E%202h)%20==%200)'"
-query=alive_no_problems
-urllib.urlretrieve(query,"nodes.txt" )
+input_file=sys.argv[4]
 api_server = xmlrpclib.ServerProxy('https://www.planet-lab.eu/PLCAPI/')
 auth = {}
 auth['Username'] = sys.argv[1] # <-- substitute your actual username here
 auth['AuthString'] = sys.argv[2] # <-- substitute your actual password here
 auth['AuthMethod'] = "password"
 
-node_list = [line.strip() for line in open("nodes.txt")]
+node_list = [line.strip() for line in open(input_file)]
 api_server.AddSliceToNodes(auth, sys.argv[3], node_list)
