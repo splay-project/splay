0a0c3d47a9443749aebd68f1c2dbca16082452ab
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-08-15 00:49:36 +0200

    support splayd-based timestamps

--- src/controller/lib/logd.rb
+++ src/controller/lib/logd.rb
@@ -18,7 +18,8 @@
 ## 
 ## You should have received a copy of the GNU General Public License
 ## along with Splayd. If not, see <http://www.gnu.org/licenses/>.
-
+require 'net/ping' #ping the jobd to compute the RTT
+include Net
 class LogdServer
 
 	@@log_max_size = SplayControllerConfig::LogMaxSize
@@ -89,21 +90,40 @@ class Logd
 	@@log_max_size = SplayControllerConfig::LogMaxSize
 	@@log_dir = SplayControllerConfig::LogDir
 	@@nat_gateway_ip = SplayControllerConfig::NATGatewayIP
-
+  @@use_splayd_timestamps = SplayControllerConfig::UseSplaydTimestamps
 	def initialize(so)
 		@so = so
 	end
 
-	def prefix(job)
-		t = Time.now
-    pfix = t.strftime("%H:%M:%S") << ".#{t.usec} " << "(#{job['splayd_id']}) "            
-		return pfix
-	end
+  def prefix_ctrl(job)
+      ts = Time.now
+      pfix = ts.strftime("%H:%M:%S") << ".#{ts.usec} " << "(#{job['splayd_id']}) "            
+  		return pfix
+  end
+  
+  def prefix(job,ts=nil)
+  		#t = Time.now
+  		if ts==nil then ts=Time.now end
+      pfix = ts.strftime("%H:%M:%S") << ".#{ts.usec} " << "(#{job['splayd_id']}) "            
+  		return pfix
+  end
+	
+  def extract_timestamp_msg(raw_msg)
+  	  $log.debug("Parsing raw message: #{raw_msg}")
+      toks=raw_msg.split(" ")
+      ts=Time.at(toks[0].to_i,toks[1].to_i)
+      msg=raw_msg[ toks[0].size + toks[1].size + 1 , raw_msg.size]
+      return ts,msg
+  end
+    #shift the timestamp wrt to the clock shift with the controller, remove the RTT as well. 
+  def adjust_ts(ts,diff,rtt)
+      ts = ts - diff - rtt
+  end
 
 	def run
 		Thread.new do
 			begin
-				$log.debug("Logd client accepted.")
+				$log.debug("Log client accepted.")
 				ip = @so.peeraddr[3]
 				# TODO replace
 				#@so.set_timeout 60
@@ -135,19 +155,43 @@ class Logd
 							splayd_selections.job_id=jobs.id AND
 							splayd_selections.splayd_id=splayds.id"
 				end
+        
+        
+        jobd_localtime = @so.gets.chop
+        
+        #Ping2::TCP.service_check = true #prevents false negatives, the host is UP for sure.
+        p1 = Net::Ping::TCP.new(:host => ip)
+        t0= Time.now
+        p1.ping #do the ping
+        rtt = (Time.now - t0)/2
+        
+        ctrl_time = Time.now
+        t= jobd_localtime.split(".")
+        jt=Time.at(t[0].to_i,t[1].to_i)        
+        difftime = ctrl_time - jt
+        $log.info("Splayd (#{job['splayd_id']}) remote-time before job: #{jt.strftime("%H:%M:%S")}.#{jt.usec.to_s} DIFF: #{difftime} RTT: #{rtt}")
+        
+         adjust_ts(jt,difftime,rtt)
         $log.debug("Logd retrieved job ref #{job_ref}")
 				if job
 					# TODO replace
 					#@so.set_timeout(24 * 3600)
 					fname = "#{@@log_dir}/#{job['id']}"
 					count = 0
+          last_ts=nil
 					begin
 #             file = File.open(fname, File::WRONLY|File::APPEND|File::CREAT, 0666) 
 						file = File.new(fname, File::WRONLY|File::APPEND|File::CREAT, 0777) 
-						file.sync = true # # This affects future operations and causes output to be written without block buffering.
+            # http://ruby-doc.org/core-1.8.7/IO.html#method-i-sync
+            # This affects future operations and causes output to be written without block buffering.
+            file.sync = true
 
 						file.flock File::LOCK_EX # synchro between processes
-						file.puts "#{prefix(job)} START_LOG"
+            if (@@use_splayd_timestamps==true) then 
+              file.puts "#{prefix(job,jt)} START_LOG"
+            else
+            	file.puts "#{prefix_ctrl(job)} START_LOG" 
+            end
 						file.flock File::LOCK_UN
 						loop do
 							msg = nil
@@ -162,16 +206,33 @@ class Logd
 
 							if msg then #prevents from errors when msg=nil, this can happen if raw=nil
 								count = count + msg.length
+                ts,m = extract_timestamp_msg(msg)
+                adjust_ts(ts,difftime,rtt)
 								file.flock File::LOCK_EX # synchro between processes
-								file.puts prefix(job) << " " << msg ##efficient string concat
+                if (@@use_splayd_timestamps==true) then 
+                  file.puts prefix(job,ts) << " " << m
+                else
+                  file.puts prefix_ctrl(job) << " " << m                
+                end
 								file.flock File::LOCK_UN
+                last_ts=ts
 							end
 
-							if count > @@log_max_size then break end
+							if count > @@log_max_size then 
+                $log.info("Log too big: #{count}, log_max_size: #{@@log_max_size}")
+                  break 
+              end
 						end
 
 						file.flock File::LOCK_EX # synchro between processes
-						file.puts "#{prefix(job)} END_LOG"
+            if (@@use_splayd_timestamps==true) then 
+                now  = Time.now
+          		  elap = now - last_ts
+          			end_log_ts = now - elap - difftime
+          			file.puts "#{prefix(job,end_log_ts)} END_LOG"
+            else
+          	    file.puts "#{prefix_ctrl(job)} END_LOG"
+            end
 						file.flock File::LOCK_UN
 					ensure
 						begin
