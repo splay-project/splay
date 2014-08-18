0c026d54041e293c3a8a1bf92451f4802ed0a6cb
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-08-18 23:25:55 +0200

    renamed testcase, split func/perf

--- src/daemon/testcases/star_40/test_parse.lua
+++ src/daemon/testcases/star_40/test_parse.lua
@@ -1,4 +1,14 @@
 require"json"
+
+--read job of nodes
+input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5"
+f,err=io.open(input_file,"r")
+local l_json=f:read("*a")
+f:close()
+print("Input file read:", input_file)
+local job,err = json.decode(l_json)
+assert(job)
+
 --read list of nodes
 input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5_list"
 f,err=io.open(input_file,"r")
@@ -8,7 +18,7 @@ print("Input file read:", input_file)
 local list,err = json.decode(l_json)
 assert(#list.nodes==40, "Expected 40 but was: "..#list.nodes)
 
---read list of nodes
+--read topology of nodes
 input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5_topology"
 f,err=io.open(input_file,"r")
 local l_json=f:read("*a")
