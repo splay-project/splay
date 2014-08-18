7833fd58571cb1106c1c53d39d44107a3f31d66b
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-08-18 18:17:42 +0200

    more asserts

--- src/daemon/testcases/star_60/test_parse.lua
+++ src/daemon/testcases/star_60/test_parse.lua
@@ -1,11 +1,18 @@
 require"json"
-input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5"
+--read list of nodes
+input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5_list"
 f,err=io.open(input_file,"r")
 local l_json=f:read("*a")
 f:close()
-print("Input file read")
+print("Input file read:", input_file)
 local list,err = json.decode(l_json)
-if err then
-	error("Fail:", err)
-end
-assert(#list.nodes==20)
+assert(#list.nodes==40, "Expected 40 but was: "..#list.nodes)
+
+--read list of nodes
+input_file="2506080138d1d5f991ede269fcdac9f7_host_splayd-99_5_topology"
+f,err=io.open(input_file,"r")
+local l_json=f:read("*a")
+f:close()
+print("Input file read:",input_file)
+local topo,err = json.decode(l_json)
+assert(topo)
