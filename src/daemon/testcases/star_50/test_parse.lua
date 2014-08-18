c3c86ac7645ea8e081f67b8e39c84a6b6817b794
splayd <splayd@splayd-99.(none)>
2014-08-18 20:13:15 +0200

    compare perfs of json/cjson

--- src/daemon/testcases/star_50/test_parse.lua
+++ src/daemon/testcases/star_50/test_parse.lua
@@ -1,10 +1,21 @@
-require"json"
+local cjson=require"cjson"
+local json=require"json"
+os=require"os"
 --read raw json (list+topology)
 input_file="b7cf318b9e87020a4900417a4a43b0b0a55e644b"
 f,err=io.open(input_file,"r")
 local l_json=f:read("*a")
 f:close()
 print("Input file read:", input_file)
-local list,err = json.decode(l_json)
+
+local x = os.clock()
+local list = json.decode(l_json)
+print(string.format("json decoded, elapsed time: %.2f\n", os.clock() - x))
+
+local x = os.clock()
+local list,err = cjson.decode(l_json)
+print(string.format("cjson decoded, elapsed time: %.2f\n", os.clock() - x))
+
+
 assert(list)
 assert(#list.nodes==50)
