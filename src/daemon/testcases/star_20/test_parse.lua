9079c258cdc856aef00f02ee01c141779d7d54a2
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-08-18 18:05:12 +0200

    do not hard-code path

--- src/daemon/testcases/star_20/test_parse.lua
+++ src/daemon/testcases/star_20/test_parse.lua
@@ -1,5 +1,6 @@
 require"json"
-f,err=io.open("/Users/veleno/workspace-git/splay/src/daemon/testcases/star_60/e451a4bc2f5a047092be27ab8761339c_host_splayd-99_5_list","r")
+input_file="e451a4bc2f5a047092be27ab8761339c_host_splayd-99_5_list"
+f,err=io.open(input_file,"r")
 local l_json=f:read("*a")
 f:close()
 print("Input file read")
