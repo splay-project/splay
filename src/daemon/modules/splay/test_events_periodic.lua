3afbcde2f61a2f389ec8301a98fbdfb0b00e2835
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-11-24 14:13:47 +0100

    use test logger

--- src/daemon/modules/splay/test_events_periodic.lua
+++ src/daemon/modules/splay/test_events_periodic.lua
@@ -2,19 +2,19 @@ require"splay.base"
 local log = require"splay.log"
 local l_o = log.new(3, "[test_events_periodic]")
 e={}
-ITERATIONS=5
+ITERATIONS=2
 events.run(function()
 	events.thread(function()
 		l_o:print("waiting for 'op' event")
 		events.wait("op")
 		assert(#e==ITERATIONS)
-		print("TEST_OK")
+		l_o:print("TEST_OK")
 		events.exit()
 	end)
 	events.periodic(1, function()
 		e[#e+1]="op"		
 		if #e==ITERATIONS then 
-			l_o:print("Periodic thread did "..ITERATIONS..", firing 'op'")
+			l_o:print("Periodic thread did "..ITERATIONS.." iterations firing 'op'")
 			events.fire("op")
 		end
 	end)
