8ca36b38ba5ab96e2b56bfd72cbe89510bb7f6c2
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-11-26 14:56:43 +0100

    same code used for test_rpc_server.lua

--- src/daemon/modules/splay/test_rpcq.lua
+++ src/daemon/modules/splay/test_rpcq.lua
@@ -1,17 +1,16 @@
 require"splay.base"
 local rpc = require"splay.rpcq"
 local log = require"splay.log"
-local l_o = log.new(3, "[test_rpcq]")
+local l_o = log.new(3, "[test_rpcq_server]")
 function pong()
 	return "pong"
 end
-events.run(function()	
-	l_o:print("run")
-    local rpc_server = rpc.server(30001)
-	assert(rpc_server)	
-	local pong_rep = rpc.call({ip="127.0.0.1",port=30001}, {"pong"})
-	assert(pong_rep)	
-	assert("pong"==pong_rep)
-	l_o:print("exiting")
+events.run(function()
+	local port=30001
+	local rpc_server_thread = rpc.server(port)
+	local pong_rep, err= rpc.call({ip="127.0.0.1",port=port}, {"pong"})	
+	assert(pong_rep=="pong")
+	assert(err==nil)
+	print("TEST_OK")
 	events.exit()
 end)
