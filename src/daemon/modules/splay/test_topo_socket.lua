07d90dff11ca9b22e18ce3dbcad748224eceedc5
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-12-03 20:33:06 +0100

    use correct logger

--- src/daemon/modules/splay/test_topo_socket.lua
+++ src/daemon/modules/splay/test_topo_socket.lua
@@ -1,20 +1,21 @@
 --setup double wrapping, topo and rs
 
 log=require"splay.log"
+l_o=log.new(3,"[test_topo_socket]")
 socket = require"socket.core"
-log:print("Socket type:",socket)
+l_o:print("Socket type:",socket)
 
 ts = require"splay.topo_socket"
 ts.l_o.level=3
 assert(ts.init({in_delay=1})) --seconds
 socket=ts.wrap(socket)
-log:print("Socket type:",socket)
+l_o:print("Socket type:",socket)
 
 rs=require"splay.restricted_socket"
 rs.l_o.level=3
 assert(rs.init({max_sockets=1024}))
 socket=rs.wrap(socket) --to gather stats on network IO and simulate in-controller deploy
-log:print("Socket type:",socket)
+l_o:print("Socket type:",socket)
 
 require"splay.base"
 
@@ -27,7 +28,7 @@ events.run(function()
 	rpc.server({ip="127.0.0.1",port=10002})
 	for i=1,10 do
 		local rtt,err=rpc.ping({ip="127.0.0.1",port=10002},12) 
-		log:print(rtt,err)
+		l_o:print(rtt,err)
 		--events.sleep(0.5)
 	end
 	rpc.stop_server(10002)
