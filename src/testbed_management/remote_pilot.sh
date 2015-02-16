4d08776fdb3550d469216486401c3f7a75a263f3
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2015-02-16 15:16:34 +0000

    send cmd in background

--- src/testbed_management/remote_pilot.sh
+++ src/testbed_management/remote_pilot.sh
@@ -1,9 +1,9 @@
-#!/opt/local/bin/bash
+#!/bin/bash
 cmd=$1
 readarray machines < cluster_hosts.txt  #requires Bash >= 4.0
 for m in ${machines[@]} 
 do
 	echo "# Executing command on host $m #"
-	ssh $m bash <<< "$cmd " 
+	ssh $m bash <<< "$cmd & " 
 	echo ""
 done
