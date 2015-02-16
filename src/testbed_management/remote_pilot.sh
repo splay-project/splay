1a48f3d89991dd4669f6f8aa7dd845c2b5335303
ralucah <ralucahalalai@gmail.com>
2015-02-16 23:07:57 +0100

    fixed remote_pilot

--- src/testbed_management/remote_pilot.sh
+++ src/testbed_management/remote_pilot.sh
@@ -3,7 +3,7 @@ cmd=$1
 readarray machines < cluster_hosts.txt  #requires Bash >= 4.0
 for m in ${machines[@]} 
 do
-	echo "# Executing command on host $m #"
-	ssh $m bash <<< "$cmd & " 
-	echo ""
+        echo "# Executing command on host $m #"
+        ssh $m  "nohup bash -c '$cmd'" &
+        echo ""
 done
