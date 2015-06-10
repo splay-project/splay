4c989099ccada6d989fdf150b3cb89402a20ab5e
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2015-06-10 15:57:39 +0200

    run all the tests

---
+++ src/controller/testsuite.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun' #not required if done at the top of each test class, here to be sure.
+require './cli-server/test_controller_api'
+require './lib/test_topology_parser.rb'
+require './lib/test_append.rb'
+require './lib/test_json_parse.rb'
+require './lib/test_min_heap.rb'
+require './lib/test_select_one.rb'
+require './lib/test_timings.rb'
+
+
