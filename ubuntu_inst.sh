10c221f771e478868673811daf816a5eaaf65439
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2015-12-03 17:46:56 +0100

    vars in .profile only read by login shell

--- ubuntu_inst.sh
+++ ubuntu_inst.sh
@@ -4,19 +4,18 @@ sudo apt-get install -y git build-essential libreadline-dev libncurses5-dev \
 lua5.1 liblua5.1-0 liblua5.1-0-dev lua-socket lua-socket-dev \
 libssl-dev lua-sec lua-sec-dev 
 
-echo "SPLAY_PATH=\"$HOME/local/lualibs/lib/?.lua\"" 		>> ~/.bashrc
-echo "SPLAY_CPATH=\"$HOME/local/lualibs/clib/?.so\""		>> ~/.bashrc
-echo "DEFAULT_LUA_PATH=\"`lua -e \"print( package.path)\"`\"" 	>> ~/.bashrc
-echo "DEFAULT_LUA_CPATH=\"`lua -e \"print( package.cpath)\"`\"" >> ~/.bashrc
-echo "LUA_PATH=\"\$SPLAY_PATH;\$DEFAULT_LUA_PATH\"" 		>> ~/.bashrc
-echo "LUA_CPATH=\"\$SPLAY_CPATH;\$DEFAULT_LUA_CPATH\""  		>> ~/.bashrc
-echo "export LUA_PATH"				>> ~/.bashrc
-echo "export LUA_CPATH"				>> ~/.bashrc
+echo "SPLAY_PATH=\"$HOME/local/lualibs/lib/?.lua\"" 			>> ~/.profile
+echo "SPLAY_CPATH=\"$HOME/local/lualibs/clib/?.so\""			>> ~/.profile
+echo "DEFAULT_LUA_PATH=\"`lua -e \"print( package.path)\"`\"" 	>> ~/.profile
+echo "DEFAULT_LUA_CPATH=\"`lua -e \"print( package.cpath)\"`\"" >> ~/.profile
+echo "LUA_PATH=\"\$SPLAY_PATH;\$DEFAULT_LUA_PATH\"" 			>> ~/.profile
+echo "LUA_CPATH=\"\$SPLAY_CPATH;\$DEFAULT_LUA_CPATH\""  		>> ~/.profile
+echo "export LUA_PATH"											>> ~/.profile
+echo "export LUA_CPATH"											>> ~/.profile
 
 git clone https://github.com/splay-project/splay.git
-cd splay/src/external_libs/lua-5.1.4/
+cd splay/src/external_libs/lua-5.1.4/ || exit;
 make linux
-cd ../../daemon/
+cd ../../daemon/ || exit;
 make
-. ~/.bashrc #in bash, '.' corresponds to 'source'
-./install.sh
+bash -l install.sh
