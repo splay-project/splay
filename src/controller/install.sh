4e3a6b27a18af9e53f4bb7969990808770af187f
raziel-carvajal <raziel.carvajal@gmail.com>
2016-08-25 17:31:05 +0200

    Script to install controller's dependencies

---
+++ src/controller/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: install.sh
+# 
+#         USAGE: ./install.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Raziel Carvajal-Gomez (RCG), raziel.carvajal@unine.ch
+#  ORGANIZATION: 
+#       CREATED: 08/25/2016 17:16
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+sudo apt-get install ruby mysql-server ruby2.3-dev libmysqld-dev
+
+sudo gem install openssl dbd-mysql mysql dbi net-ping nokogiri algorithms sequel
